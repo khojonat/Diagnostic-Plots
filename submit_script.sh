142f3ce544139c808cbed5ca999af213a58126b1
khojonat <khojonat@msu.edu>
2026-05-06 14:38:05 -0400

    Updated slurm script

--- submit_script.sh
+++ submit_script.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #SBATCH -J diagn
-#SBATCH -A torrey-group
+#SBATCH -A torrey-group # Change to your allocation
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=01:00:00
 #SBATCH -p standard
+#SBATCH --array=1-10 # Add run numbers here
 #SBATCH --output=logs/diagnose_%j.out 
 #SBATCH --error=logs/diagnose_%j.err 
-#SBATCH --mail-user=yja6qa@virginia.edu
+#SBATCH --mail-user=yja6qa@virginia.edu #  Change to your email
 #SBATCH --mail-type=BEGIN,END,FAIL
 
+mkdir -p logs
+
 module purge
-module load miniforge
-conda activate kho_env
+module load miniforge  # Loading conda on Rivanna
+conda activate kho_env # Change to your conda env
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export SNAP_NUM=90
 
-python run_all_diagnostics.py /project/torrey-group/jkho/Kho_FIRE_MW_Suite/run_0/zoom/RUNs/output/ 90 --box-num 3 
+python run_all_diagnostics.py $SLURM_ARRAY_TASK_ID $SNAP_NUM 
