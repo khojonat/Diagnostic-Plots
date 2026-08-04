cf7064d63f55ec327aed81b586113520fe8bd084
Jonathan Kho <yja6qa@virginia.edu>
2026-08-04 08:11:11 -0400

    Updated slurm script, .gitignore

--- submit_script.sh
+++ submit_script.sh
@@ -6,7 +6,6 @@
 #SBATCH --mem=64G
 #SBATCH --time=01:00:00
 #SBATCH -p standard
-#SBATCH --array=1-10 # Add run numbers here
 #SBATCH --output=logs/diagnose_%j.out 
 #SBATCH --error=logs/diagnose_%j.err 
 #SBATCH --mail-user=yja6qa@virginia.edu #  Change to your email
@@ -14,9 +13,5 @@
 
 mkdir -p logs
 
-module purge
-module load miniforge  # Loading conda on Rivanna
-conda activate kho_env # Change to your conda env
-
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-python run_all_diagnostics.py $SLURM_ARRAY_TASK_ID
+python run_finished_diagnostics.py
